0699855296f3e7c576ce9915443c306586f38d18
David Teller <david.teller@pasqal.com>
2025-02-05 09:29:18 +0100

    WIP

--- test/emu_sv/test_custom_callbacks.py
+++ test/emu_sv/test_custom_callbacks.py
@@ -112,7 +112,6 @@ def test_custom_energy_and_variance_and_second():
     )
     expected_variance = 3.67378968943955
 
-
     assert energy_variance == approx(expected_variance, abs=1e-7)
 
     second_momentum_energy_mock = MagicMock(spec=SecondMomentOfEnergy)
